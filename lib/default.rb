9eaeb2ac0bfbc8be67b91e9115aad96dc99c9a2f
Holger Just <web@meine-er.de>
2010-08-30 12:18:20 +0200

    Major tests and tryouts

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,15 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+def title
+  if @item[:title]
+    "#{@item[:title]} || #{site_title}"
+  else
+    site_title
+  end
+end
+  
+def site_title
+  "meine erde"
+end
+    
