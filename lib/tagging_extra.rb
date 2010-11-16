5d066961ff3305aeb8114988383036d6c85e072e
Holger Just <web@meine-er.de>
2010-11-16 15:20:31 +0100

    Reformat comments

--- lib/tagging_extra.rb
+++ lib/tagging_extra.rb
@@ -4,10 +4,10 @@ module TaggingExtra
 
   require 'set'
 
-  #Returns all the tags present in a collection of items.
-  #The tags are only present once in the returned value.
-  #When called whithout parameters, all the site items
-  #are considered.
+  # Returns all the tags present in a collection of items.
+  # The tags are only present once in the returned value.
+  # When called whithout parameters, all the site items
+  # are considered.
   def tag_set(items=nil, include_meta=false) 
     items = @items if items.nil?
     items = [items] unless items.is_a? Array
@@ -16,26 +16,26 @@ module TaggingExtra
     include_meta ? tags : tags.select{|tag| not tag.include? ":"}
   end
 
-  #Return true if an items has a specified tag
+  # Return true if an items has a specified tag
   def has_tag?(item, tag)
     return false if item[:tags].nil?
     item[:tags].include? tag
   end
 
-  #Finds all the items having a specified tag.
-  #By default the method search in all the site
-  #items. Alternatively, an item collection can
-  #be passed as second (optional) parameter, to
-  #restrict the search in the collection.
+  # Finds all the items having a specified tag.
+  # By default the method search in all the site
+  # items. Alternatively, an item collection can
+  # be passed as second (optional) parameter, to
+  # restrict the search in the collection.
   def items_with_tag(tag, items=nil)
     items = @items if items.nil?
     items.select { |item| has_tag?( item, tag ) }
   end
   
-  #Count the tags in a given collection of items.
-  #By default, the method counts tags in all the
-  #site items.
-  #The result is an hash such as: { tag => count }.
+  # Count the tags in a given collection of items.
+  # By default, the method counts tags in all the
+  # site items.
+  # The result is an hash such as: { tag => count }.
   def count_tags(items=nil)
     items ||= @items
     items = [items] unless items.is_a? Array
@@ -50,11 +50,11 @@ module TaggingExtra
     result
   end
 
-  #Sort the tags of an item collection (defaults
-  #to all site items) in 'n' classes of rank.
-  #The rank 0 corresponds to the most frequent
-  #tags. The rank 'n-1' to the least frequents.
-  #The result is a hash such as: { tag => rank }
+  # Sort the tags of an item collection (defaults
+  # to all site items) in 'n' classes of rank.
+  # The rank 0 corresponds to the most frequent
+  # tags. The rank 'n-1' to the least frequents.
+  # The result is a hash such as: { tag => rank }
   def rank_tags(n, items=nil) 
     items ||= @items
     count = count_tags( items )
